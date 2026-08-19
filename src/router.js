fe582c289361e960e60530a225fffe2fd3b49be3
bubblehhhh <87700625+bubblehhhh@users.noreply.github.com>
2026-08-19 22:18:17 +0800

    Add competency-driven mentorship workflow

--- src/router.js
+++ src/router.js
@@ -1,19 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import Login from './views/Login.vue'
 import Dashboard from './views/Dashboard.vue'
-import TaskCenter from './views/TaskCenter.vue'
+import TaskCenter from './views/TaskCenterEnhanced.vue'
 import SmartInput from './views/SmartInput.vue'
 import Growth from './views/Growth.vue'
 import WorkLibrary from './views/WorkLibrary.vue'
 import CareCenter from './views/CareCenter.vue'
 import Toolbox from './views/Toolbox.vue'
+import CapabilityCenter from './views/CapabilityCenter.vue'
 
-const router = createRouter({ history: createWebHistory(), routes: [
+const router = createRouter({ history: location.protocol === 'file:' ? createWebHashHistory() : createWebHistory(), routes: [
   { path: '/login', component: Login, meta: { public: true } },
   { path: '/', component: Dashboard },
   { path: '/tasks', component: TaskCenter },
   { path: '/smart-input', component: SmartInput },
   { path: '/growth', component: Growth },
+  { path: '/capabilities', component: CapabilityCenter },
   { path: '/work-library', component: WorkLibrary },
   { path: '/care', component: CareCenter },
   { path: '/toolbox', component: Toolbox }
